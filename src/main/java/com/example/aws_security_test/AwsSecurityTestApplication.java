29dd75d7aefd707c39a5a8de1ab05491c6089ebf
홍혁준 <jjj8337721@naver.com>
2022-05-09 14:52:09 +0900

    Initial commit

---
+++ src/main/java/com/example/aws_security_test/AwsSecurityTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_security_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSecurityTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSecurityTestApplication.class, args);
+    }
+
+}
