eda878063b48cdb1d6a8497e434ca8ef5b97b10f
홍혁준 <jjj8337721@naver.com>
2022-06-22 18:09:22 +0900

    deploy.sh Creat5

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #TIMESTAMP 변수 생성 후 현재 시각으로 초기화
 TIMESTAMP=`date +%s`
@@ -15,7 +15,6 @@ echo "docker login completed"
 
 sudo docker build -t ${DOCKER_IMAGE} .
 echo "docker build completed"
-
 sudo docker push ${DOCKER_IMAGE}
 echo "docker push completed"
 
